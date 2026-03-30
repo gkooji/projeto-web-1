1732adf10ff4c9c12103fbcca45257fe0eefd313
Kooji <labsfiap@fiap.com.br>
2026-03-30 09:15:52 -0300

    feat: Declarações e variaveis

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,46 @@
-alert("Hello World !!!")
+// declarações e variaveis
+// var, let e const
+
+var nome="Fiap";
+console.log(nome);
+
+var nome="Teste";
+console.log(nome);
+
+if(true){
+    let apelido="fiapinho";
+   console.log(apelido);
+}
+
+const taxaJuros=0.05;
+console.log(taxaJuros)
+//taxaJuros=1.5;
+
+var exemplo="ola-dev-var";
+console.log(typeof exemplo)
+let exemplo1="ola-dev-let";
+console.log(exemplo1)
+const exemplo2="ola-dev-const";
+console.log(exemplo2)
+
+let exemplo3=10;
+console.log(typeof exemplo3)
+
+let exemplo4=true;
+console.log(typeof exemplo4);
+
+let exemplo5={nome1:"teste"}
+console.log(typeof exemplo5)
+
+let exemplo6=["java", "c#", "php"]
+console.log(typeof exemplo6)
+
+// valor undefined
+let exemplo7;
+console.log(exemplo7)
+
+// valor null
+
+let exemplo8 =null;
+console.log(exemplo8)
+
