571259f63d1ade405907cc54de51bd4537b2c108
Guilherme <guikooji11@hotmail.com>
2026-03-30 09:58:25 -0300

    feat: exercicios de operadores

--- src/js/script.js
+++ src/js/script.js
@@ -57,9 +57,36 @@ console.log(valor1 / valor2);
 // operadores logicos
 // = atribuição
 // && = E (todas as condi)
+// || = OU (uma das condição tem que ser verdadeira)
+
 
 console.log(valor1 <valor2);
 console.log(valor1 > 10 && valor2 <10);
 console.log(valor2 < 100 || valor1 >10);
 console.log(valor1 == valor2);
 console.log(valor2 === valor1);
+
+// ex1
+
+const v=3;
+const p=4;
+
+const precofinal = p - v;
+console.log("o preço final com desconto é", precofinal);
+
+// ex2
+
+let a=20;
+let b=10;
+
+let comparar= (a==20 && b>= 10);
+
+console.log("o resultado é",comparar)
+
+// ex3
+
+const usuariologado=20
+const tempotoken= 10
+
+const transação = (usuariologado ===20 && tempotoken >=10)
+console.log("transação autorizada",transação)
